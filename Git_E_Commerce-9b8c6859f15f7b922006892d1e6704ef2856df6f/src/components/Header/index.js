eff59c047bd925df4b61bb015185679ccc1151f6
harendra <jeaturajabundela@gmail.com>
2024-11-23 16:20:09 +0530

    SearchBox takk

--- Git_E_Commerce-9b8c6859f15f7b922006892d1e6704ef2856df6f/src/components/Header/index.js
+++ Git_E_Commerce-9b8c6859f15f7b922006892d1e6704ef2856df6f/src/components/Header/index.js
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/th (1).jpg";
+import { Button } from "react-bootstrap"; // Ensure this is correct
+import { MdMenuOpen } from "react-icons/md";
+import SearchBox from "../SearchBox";
+
 const Header = () => {
   return (
     <>
-      <header className="d-flex align-items-centre">
+      <header className="d-flex align-items-center">
         <nav className="container-fluid w-100">
-          <div className="row d-flex align-items-centre">
-            <div className="col-xs-3">
-              {/*logo warapper*/}
-              <Link to={"/"} className="d-flex align-items-centre logo">
-                <img src={logo} />
-                <span className=" ml-2 mt-1"> ECOM</span>
+          <div className="row d-flex align-items-center">
+
+            <div className="col-sm-2 part-1">
+              <Link to={"/"} className="d-flex align-items-center logo">
+                <img src={logo} alt="Logo" />
+                <span className="ml-2 mt-1"> ECOM</span>
               </Link>
+            </div>
+
+            <div className="col-xs-3  d-flex align-items-center part-2 pl-4">
+              <Button className="rounded-circle mr-3">
+                <MdMenuOpen />
+              </Button>
+              <SearchBox/>
             </div>
           </div>
         </nav>
---
+++ Git_E_Commerce-9b8c6859f15f7b922006892d1e6704ef2856df6f/src/components/SearchBox/index.js
@@ -0,0 +1,17 @@
+import { IoSearch } from "react-icons/io5";
+//           <IoSearch className="mr-2 mt-3 ml-1" />  
+
+
+
+
+
+const SearchBox = () =>{
+    return (
+        <div className="searchBox position-relative d-flex align-item-centre">
+            <input type="text" className="form-control" placeholder="Search..."/>
+        </div>
+        );
+}
+
+
+export default SearchBox;
